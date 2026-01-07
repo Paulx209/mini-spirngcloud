702a50160c20d32325d31202327ffe62dfca400b
get <249628024@qq.com>
2026-01-07 19:48:47 +0800

    feat(mini-springCloud):新增module -> mini-springCloud-provider-example

---
+++ mini-springCloud-examples/mini-springCloud-provider-example/src/main/java/com/getian/providerexample/MiniSpringCloudProviderExampleApplication.java
@@ -0,0 +1,13 @@
+package com.getian.providerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSpringCloudProviderExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniSpringCloudProviderExampleApplication.class, args);
+    }
+
+}
