148d33b47cea697c107aece606bcf08e4bd6e5bc
etge <etge@iflytek.com>
2026-01-08 16:29:09 +0800

    feat(mini-springCloud):[服务注册]:新增服务注册核心类

---
+++ mini-spring-cloud-tutu-discovery/src/main/java/com/github/cloud/tutu/registry/TutuAutoServiceRegistration.java
@@ -0,0 +1,41 @@
+package com.github.cloud.tutu.registry;
+
+import org.springframework.cloud.client.serviceregistry.AbstractAutoServiceRegistration;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.cloud.client.serviceregistry.ServiceRegistry;
+
+/**
+ * 服务自动注册
+ */
+public class TutuAutoServiceRegistration extends AbstractAutoServiceRegistration<Registration> {
+    private TutuRegistration tutuRegistration;
+
+    protected TutuAutoServiceRegistration(ServiceRegistry<Registration> serviceRegistry,TutuRegistration tutuRegistration){
+        super(serviceRegistry,null);
+        this.tutuRegistration = tutuRegistration;
+    }
+
+    @Override
+    protected Registration  getRegistration() {
+        if(tutuRegistration.getPort() < 0){
+            //设置服务端口号
+            tutuRegistration.setPort(this.getPort().get());
+        }
+        return tutuRegistration;
+    }
+    @Override
+    protected Object getConfiguration() {
+        return tutuRegistration.getTutuDiscoveryProperties();
+    }
+
+    @Override
+    protected boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    protected Registration getManagementRegistration() {
+        return null;
+    }
+
+}
